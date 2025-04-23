0fdc693e5f47aee16cb07e77beabbbab128ea1c8
Harsh <203636452+Harsh00CE@users.noreply.github.com>
2025-04-23 10:39:34 +0530

    jkh

--- backend/schemas/demoWalletSchema.js
+++ backend/schemas/demoWalletSchema.js
@@ -11,44 +11,44 @@ const demoWalletSchema = new Schema({
     balance: {
         type: Number,
         required: [true, "Balance is required"],
-        default: 50000,
         set: (value) => Number(value.toFixed(3)),
+        default: 50000,
     },
     equity: {
         type: Number,
         required: [true, "Equity is required"],
-        default: 50000,
         set: (value) => Number(value.toFixed(3)),
+        default: 50000,
     },
     available: {
         type: Number,
         required: [true, "Available is required"],
-        default: 50000,
         set: (value) => Number(value.toFixed(3)),
+        default: 50000,
     },
     margin: {
         type: Number,
         required: [true, "Margin is required"],
-        default: 0,
         set: (value) => Number(value.toFixed(3)),
+        default: 0,
     },
     marginLevel: {
         type: Number,
         required: [true, "Margin level is required"],
-        default: 0,
         set: (value) => Number(value.toFixed(3)),
+        default: 0,
     },
     leverage: {
         type: Number,
-        default: 1,
         required: true,
         min: 1,
+        default: 1,
     },
     pl: {
         type: Number,
         required: [true, "pl is required"],
-        default: 0,
         set: (value) => Number(value.toFixed(3)),
+        default: 0,
     },
 });
 
